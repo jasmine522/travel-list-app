6b4cba133b667257a76585cb625137b2676e93e8
Jasmine Duruji <jasminedrj@gmail.com>
2022-02-24 22:33:49 -0500

    Added Files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from "react"
-import BucketList from "./Components/BucketList"
-import Home from "./Components/Home"
-import Login from "./Components/Login"
-import Places from "/Components/Places"
-import SignUp from "./Components/SignUp"
+import React from "react";
+import BucketList from "./Components/BucketList";
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+import Places from "./Components/Places";
+import SignUp from "./Components/SignUp";
 
 function App() {
   return (
-    <h1>Hey yall!</h1>
+    <div className="App">
+      <BucketList />
+      <Home />
+      <Login />
+      <Places />
+      <SignUp />
+    </div>
   )
 }
 
