e8cc942edec506bb23c89391463e5dfd87994810
Jasmine Duruji <jasminedrj@gmail.com>
2022-02-24 22:39:40 -0500

    React Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route  } from "react-router-dom";
 import BucketList from "./Components/BucketList";
 import Home from "./Components/Home";
 import Login from "./Components/Login";
